3a10e8d017bb1c7d3f6f04c1b0c7dbd690fa1953
Michael <michael.leonov96@mail.ru>
2023-05-16 20:40:28 +0300

    some changes

--- client/src/components/add-card/index.tsx
+++ client/src/components/add-card/index.tsx
@@ -25,7 +25,9 @@ const AddCard = () => {
 
   useEffect(() => {
     if (formState.isSubmitSuccessful) {
-      reset({ title: '', description: '' })
+      setTimeout(() => {
+        reset({ title: '', description: '' })
+      }, 2000)
     }
   }, [formState, reset])
 
@@ -39,7 +41,10 @@ const AddCard = () => {
       <div className='container mx-auto'>
         <h1>Форма добавление новой карточки</h1>
 
-        <form className='flex flex-col items-center gap-y-2' onSubmit={handleSubmit(onSubmit)}>
+        <form
+          className='relative flex flex-col items-center gap-y-2'
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <label htmlFor='title'>Заголовок карточки </label>
           <input
             type='text'
@@ -66,8 +71,35 @@ const AddCard = () => {
 
           {errors.description && <p className='error'>{errors.description.message}</p>}
 
-          <button className='main-button mb-4' disabled={isAdding}>
-            Добавить карточку
+          {formState.isSubmitSuccessful && (
+            <p className='text-sm font-semibold text-center text-emerald-500'>
+              Форма успешно отправлена!
+            </p>
+          )}
+
+          <button className='main-button mb-4 inline-flex items-center px-4' disabled={isAdding}>
+            {isAdding ? (
+              <>
+                <svg className='animate-spin h-5 w-5 mr-3 text-white' viewBox='0 0 24 24'>
+                  <circle
+                    className='opacity-25'
+                    cx='12'
+                    cy='12'
+                    r='10'
+                    stroke='currentColor'
+                    strokeWidth='4'
+                  />
+                  <path
+                    className='opacity-75'
+                    fill='currentColor'
+                    d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+                  />
+                </svg>
+                <>Обработка...</>
+              </>
+            ) : (
+              <>Добавить карточку</>
+            )}
           </button>
         </form>
       </div>
