1be4b94a897c147a30f5fabb822a6b1273e7d42e
michael-leonov <michael.leonov96@mail.ru>
2023-05-16 17:42:41 +0300

    delete adaptive const

--- client/src/utils/consts.ts
+++ client/src/utils/consts.ts
@@ -2,15 +2,3 @@ export const SIGNUP_ROUTE = '/signup'
 export const SIGNIN_ROUTE = '/signin'
 export const CARDLIST_ROUTE = '/'
 export const FORM_ROUTE = '/form'
-
-const size = {
-  tablet: '768px',
-  laptop: '1024px',
-  desktop: '1440px',
-}
-
-export const device = {
-  tablet: `(min-width: ${size.tablet})`,
-  laptop: `(min-width: ${size.laptop})`,
-  desktop: `(min-width: ${size.desktop})`,
-}
