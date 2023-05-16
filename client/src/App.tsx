693667e21b993f9fbe5698519b8e9f4e7ad33866
Michael <michael.leonov96@mail.ru>
2023-05-16 11:41:33 +0300

    set up client

---
+++ client/src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react'
+import Header from './components/header'
+import AppRoutes from './routes'
+import { useAppDispatch } from './redux/hooks'
+import { setUser } from './redux/features/userSlice'
+import { useCheckQuery } from './redux/services/userApi'
+
+const App = () => {
+  const dispatch = useAppDispatch()
+
+  const user = JSON.parse(localStorage.getItem('user') || '{}')
+
+  const { data, isSuccess } = useCheckQuery()
+
+  useEffect(() => {
+    if (user?.token) {
+      if (isSuccess)
+        dispatch(setUser({ name: user.name, token: data.token, id: user.id, isAuth: true }))
+    } else {
+      dispatch(setUser(user))
+    }
+  }, [])
+
+  return (
+    <>
+      <Header />
+      <AppRoutes />
+    </>
+  )
+}
+
+export default App
